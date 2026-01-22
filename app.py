f8a8d79a04476e8534af44a7321648a8dc7a0427
Yacine Klikel <yacine@Mac.lan>
2026-01-22 21:41:54 +0100

    Sauvegarde de mon brouillon avant fusion Albin

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ def vote_card():
     return render_template('card_fragment.html', carte=nouvelle_carte, current_deck=deck_name)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001 )
